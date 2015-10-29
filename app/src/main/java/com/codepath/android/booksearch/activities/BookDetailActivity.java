41a6ced8022017f57fcf4e5eaf60afbda052c634
Devin Breen <dbreen@groupon.com>
2015-10-29 17:51:05 -0500

    working share button for the image

--- app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
+++ app/src/main/java/com/codepath/android/booksearch/activities/BookDetailActivity.java
@@ -3,17 +3,23 @@ package com.codepath.android.booksearch.activities;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.codepath.android.booksearch.R;
+import com.codepath.android.booksearch.lib.MediaStoreHelper;
 import com.codepath.android.booksearch.models.Book;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
 public class BookDetailActivity extends AppCompatActivity {
+
+    private ShareActionProvider miShareAction;
 
     private static class ViewHolder {
         private ImageView ivBookCover;
@@ -21,12 +27,14 @@ public class BookDetailActivity extends AppCompatActivity {
         private TextView tvAuthor;
     }
 
+    private ViewHolder viewHolder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
         // Fetch views
-        ViewHolder viewHolder = new ViewHolder();
+        viewHolder = new ViewHolder();
 
         viewHolder.ivBookCover = (ImageView) findViewById(R.id.ivBookCover);
         viewHolder.tvTitle = (TextView) findViewById(R.id.tvTitle);
@@ -34,19 +42,43 @@ public class BookDetailActivity extends AppCompatActivity {
 
         // Extract book object from intent extras
         Intent intent = getIntent();
-        Book book = (Book) intent.getParcelableExtra("book");
+        Book book = intent.getParcelableExtra("book");
+
+        // set title
+        getSupportActionBar().setTitle(book.getTitle());
 
         // Use book object to populate data into views
-        Picasso.with(getApplicationContext()).load(Uri.parse(book.getLargeCoverUrl())).placeholder(R.drawable.ic_nocover).into(viewHolder.ivBookCover);
         viewHolder.tvTitle.setText(book.getTitle());
         viewHolder.tvAuthor.setText(book.getAuthor());
+        Picasso.with(this).load(Uri.parse(book.getLargeCoverUrl())).placeholder(R.drawable.ic_nocover).into(viewHolder.ivBookCover, new Callback() {
+            @Override
+            public void onSuccess() {
+                setupShareIntent();
+            }
+
+            @Override
+            public void onError() {
+            }
+        });
     }
 
+    private void setupShareIntent() {
+        ImageView ivImage = viewHolder.ivBookCover;
+        Uri uri = MediaStoreHelper.getImage(ivImage, getContentResolver());
+        Intent shareIntent = new Intent();
+        shareIntent.setAction(Intent.ACTION_SEND);
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+        shareIntent.setType("image/*");
+        miShareAction.setShareIntent(shareIntent);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_book_detail, menu);
+
+        MenuItem shareButton = menu.findItem(R.id.action_share);
+        miShareAction = (ShareActionProvider) MenuItemCompat.getActionProvider(shareButton);
         return true;
     }
 
@@ -56,11 +88,6 @@ public class BookDetailActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
